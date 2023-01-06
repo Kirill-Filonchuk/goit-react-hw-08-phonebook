e8766ae09ca9daa78e832938d80f9eb4a66c1c26
Kirill Filonchuk <kir.realtor@gmail.com>
2023-01-06 23:29:02 +0200

    auth completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { useEffect, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+// import Home from '../pages/Home';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const ContactsPage = lazy(() => import('../pages/Contacts'));
+
 export const App = () => {
+  // useEffect(() => {}, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} /> {/* R_R*/}
+        <Route path="/login" element={<LoginPage />} /> {/* R_R*/}
+        <Route path="/contacts" element={<ContactsPage />} /> {/* P_R*/}
+      </Route>
+    </Routes>
   );
 };
