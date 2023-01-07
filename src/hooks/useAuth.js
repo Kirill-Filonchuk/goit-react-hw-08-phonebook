4e1907da51a3053ebc2a4f45b37336791306badd
Kirill Filonchuk <kir.realtor@gmail.com>
2023-01-07 16:00:01 +0200

    fixing commit

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux';
+import {
+  selectIsLoggedIn,
+  selectUser,
+  selectIsRefreshing,
+} from 'redux/auth/selectors';
+
+export const useAuth = () => {
+  console.log('executing useAuth');
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const user = useSelector(selectUser);
+
+  return { isLoggedIn, isRefreshing, user };
+};
